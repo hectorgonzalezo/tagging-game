367c62a765c7223d6fb98d7bffc17f79768042e0
Hector Gonzalez <gorozco.hector@gmail.com>
2022-10-20 16:30:15 -0400

    fix bullseye
    It wasn't rendered in the lower part of the image

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import '../styles/instructionsStyle.css';
 
-function Instructions({ time, restartFunc }) {
+function Instructions() {
   return (
     <div id="instructions-outer">
       <div id="instructions-inner">
-      <ul name="instructions" id="instructions-dropdown">
-      <li>
-        <h2>?</h2>
-        <ul>
-        <li>Find the characters shown in the menu located on the top right of the screen.</li>
-        <li>Click anywhere in the image and select the character from the menu.</li>
-        <li>Can you make your way into the high scores?</li>
+        <ul name="instructions" id="instructions-dropdown">
+          <li>
+            <h2>?</h2>
+            <ul>
+              <li>
+                Find the characters shown in the menu located on the top right
+                of the screen.
+              </li>
+              <li>
+                Click anywhere in the image and select the character from the
+                menu.
+              </li>
+              <li>Can you make your way into the high scores?</li>
+            </ul>
+          </li>
         </ul>
-      </li>
-    </ul>
       </div>
     </div>
   );
